304625099e6d55c04b67ccac506f50b227e72d9f
Bierloga <90850242+Bierloga@users.noreply.github.com>
2021-10-31 21:09:31 +0100

    Add files via upload

---
+++ Iterators/script.js
@@ -0,0 +1,19 @@
+let colors = ["yellow", "blue", "red", "orange"]
+console.log(colors);
+
+let w = 0
+while (w < colors.length) {
+    console.log(colors[w])
+    w++
+};
+
+for (i = 0; i < colors.length; i++) {
+    console.log(colors[i])
+};
+
+colors.forEach(element => console.log(element));
+
+
+// 1. While loop 5 regels, for loop 3 regels
+// 2. 1 regel
+// 
